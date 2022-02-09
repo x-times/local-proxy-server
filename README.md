89112fed76ea4ba8a7d4931a46569bde8d31585c
MwumLi <mwumli@hotmail.com>
2022-02-09 13:12:59 +0800

    feat: 代理请求到本地文件或线上服务器

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # local-proxy-server
-本地文件代理服务器
+
+一个轻量本地文件代理服务器。  
+
+如果你希望一部分资源获取/API请求根据某种规则映射到本地文件, 那么你需要它  
+如果你还希望另外一部分走线上服务, 那么你需要它  
+
+## Usage
+
+``` bash
+# 安装
+$ npm i -g local-proxy-server
+
+# 默认启动在 3000 端口, 并且加载当前执行目录的 conf.js
+$ local-proxy-server
+
+# 指定配置文件
+$ local-proxy-server -c conf.js
+```
+
+## 配置参考
+
+
+``` javascript
+const path = require('path');
+const cwd = process.cwd();
+const applictionPath = function (...paths) {
+  path.join(cwd, 'application', ...paths);
+}
+module.exports = {
+  server: {
+    port: 3003, // 默认监听 3000
+    host: '0.0.0.0' // 监听所有网卡, 默认 localhost
+  },
+  localRules: [
+    // 代理 /api/v1 的接口到 application/api/ 下的 json 文件, 映射规则见 filepath
+    {
+      path: '/api/v1/(.*)',
+      filepath: (ctx) => {
+        const filepath = `${ctx.method}__${ctx.path.replace(/\//g, '__')}.json`;
+        return applictionPath('api', filepath);
+      }
+    },
+    // 映射资源路径到具体文件
+    {
+      path: '(.*)',
+      filepath: (ctx) => {
+        const pathFunc = v => application('web', v);
+        if (path.extname(ctx.path).length) return pathFunc(ctx.path);
+        return pathFunc()
+      },
+    },
+  ],
+  // 对于所有没有匹配到映射对目录下的 index.html, 适应 SPA 应用
+  historyApiFallback: applictionPath('web'),
+  // 可以对部门 API 代理已有的服务上
+  proxy: {
+    '/api/v2': {
+      target: 'https://10.48.73.210:8443',
+      changeOrigin: true,
+      secure: false
+    }
+  }
+}
+```
+
---
+++ server.js
@@ -0,0 +1,162 @@
+const Koa = require('koa');
+const koaBody = require('koa-body');
+const path = require('path');
+const { pathToRegexp } = require("path-to-regexp");
+const fs = require('fs')
+const os = require('os')
+const util = require('util')
+const c2k = require('koa2-connect')
+const createError = require('http-errors')
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const argv = require('minimist')(process.argv.slice(2));
+const confFile = parseConfFilepath(argv.c);
+const defaultConf = {
+  server: {
+    port: '3000'
+  }
+};
+
+const { localRules, server, proxy, historyApiFallback } = fs.existsSync(confFile) ?require(confFile) : defaultConf;
+
+const app = new Koa();
+app.use(koaBody());
+// 代理到服务器
+if (proxy) {
+  proxyService(app, proxy)
+}
+
+// 代理文件到本地文件
+if (localRules) {
+  proxyLocal(app, localRules)
+}
+
+if (historyApiFallback) {
+  console.log(`History mode Enabled: ${historyApiFallback}`)
+  let index = path.extname(historyApiFallback).length ? '' : `index.html`;
+  app.use(async (ctx, next) => {
+    return await sendFile(ctx, historyApiFallback, { index });
+  });
+}
+
+const port = server.port;
+const host = server.host || '127.0.0.1';
+app.listen(port, host, async (err) => {
+  if (err) throw err
+  let ip = host;
+  if (host === '0.0.0.0') {
+    console.log('Listen all interface');
+    ip = getAllInterfaces()[0].ip;
+  }
+  console.log(`You can visit http://${ip}:${port}`)
+});
+
+
+function proxyService(app, proxy) {
+  for (const api of Object.keys(proxy)) {
+    let currentProxy = proxy[api];
+    if (typeof currentProxy === 'string') {
+      currentProxy = {
+        target: currentProxy
+      };
+    }
+    app.use(c2k(createProxyMiddleware(api, currentProxy)));
+  }
+}
+
+function proxyLocal(app, localRules) {
+  localRules.forEach(rule => rule.pathReg = pathToRegexp(rule.path));
+  app.use(async (ctx, next) => {
+    const { path } = ctx.request;
+    const match = localRules.find(({pathReg}) => pathReg.test(path));
+    if (match) {
+      const filepath = match.filepath(ctx);
+      return await sendFile(ctx, filepath)
+        .catch((er) => {
+          if (er.code === 'ENOENT' && er.status === 404) {
+            ctx.status = 404
+            ctx.body = 'Not Found1'
+          }
+        })
+    }
+    await next();
+  });
+}
+
+function parseConfFilepath(conf = './conf.js') {
+  if (path.isAbsolute(conf)) {
+    return conf;
+  }
+  return path.join(process.cwd(), conf);
+}
+
+/**
+ * 获取当前机器所有网卡的IP
+ * @returns {Array} 网卡列表
+ */
+function getAllInterfaces() {
+  const nets = os.networkInterfaces();
+  const results = [];
+  
+  for (const name of Object.keys(nets)) {
+      for (const net of nets[name]) {
+          // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+          if (net.family === 'IPv4' && !net.internal) {
+              if (!results[name]) {
+                  results[name] = [];
+              }
+              results.push({
+                name,
+                ip: net.address
+              });
+          }
+      }
+  }
+  return results;
+}
+
+/**
+ * 发送静态文件
+ * @param {*} ctx 
+ * @param {*} filepath 
+ * @param {*} param2 
+ * @returns 
+ */
+async function sendFile(ctx, filepath, { index, maxAge = 0, immutable = false }) {
+  const stat = util.promisify(fs.stat);
+  let stats
+  try {
+    stats = await stat(filepath)
+
+    // Format the path to serve static file servers
+    // and not require a trailing slash for directories,
+    // so that you can do both `/directory` and `/directory/`
+    if (stats.isDirectory()) {
+      if (index) {
+        filepath += `/${index}`
+        stats = await stat(filepath)
+      } else {
+        return next();
+      }
+    }
+  } catch (err) {
+    const notfound = ['ENOENT', 'ENAMETOOLONG', 'ENOTDIR']
+    if (notfound.includes(err.code)) {
+      throw createError(404, err)
+    }
+    err.status = 500
+    throw err
+  }
+
+  ctx.set('Content-Length', stats.size);
+  if (!ctx.response.get('Last-Modified')) ctx.set('Last-Modified', stats.mtime.toUTCString())
+  if (!ctx.response.get('Cache-Control')) {
+    const directives = [`max-age=${(maxAge / 1000 | 0)}`]
+    if (immutable) {
+      directives.push('immutable')
+    }
+    ctx.set('Cache-Control', directives.join(','))
+  }
+  if (!ctx.type) ctx.type = path.extname(filepath)
+  ctx.body = fs.createReadStream(filepath)
+}
